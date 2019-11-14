30e980ccef61526e37120311ceadfae528d1fba3
blittable <blittable-bits@gmail.com>
2019-11-14 11:07:51 +0700

    Removed external dependencies

--- essembly-wasi/readme.md
+++
@@ -1,2 +0,0 @@
-## Overview
-The essembly-wasi crate is a proving-ground for compiling essembly-core to wasi 
--- essembly-wasi/src/main.rs
+++
@@ -1,9 +0,0 @@
-use accounts::tax::*;
-use essembly_core::entity::*;
-
-fn main() {
-    println!("Hello, world!");
-
-    let result = calc_tax(2.0, 0.05);
-    println!("Tax Result: {:?}", result);
-}
